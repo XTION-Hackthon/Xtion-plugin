b09e3c32813ae73760b36b0c8e5a738dacd262a0
GH <BoneAsh@iCloud.com>
2025-09-28 19:42:24 +0800

    Initial Commit

---
+++ Xtion-plugin/Xtion_pluginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xtion_pluginApp.swift
+//  Xtion-plugin
+//
+//  Created by GH on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Xtion_pluginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
